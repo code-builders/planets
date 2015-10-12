6e465b5f8fc2fbe96c704a10dfe1446a235f33a6
smurfik <smurfik@mlpinit.com>
2015-10-12 14:31:51 -0700

    add info about Mercury

--- tammy_planets.rb
+++ tammy_planets.rb
@@ -15,8 +15,8 @@ class Planet
     @radius
   end
 
-  def mass
-    @mass
+  def age
+    @age
   end
 
   def distance_from_the_sun
@@ -25,5 +25,17 @@ class Planet
 
 end
 
-planets = ["Mercury", "Mars", "Pluto", "Earth", "Venus", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# planets = ["Mercury", "Mars", "Pluto", "Earth", "Venus", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+mercury = Planet.new("Mercury", 1516, "4.5", 35980000)
+
+planets = {
+  "Mercury" => mercury
+  }
+
+puts "What would you like to know about?"
+answer = gets.chomp.capitalize
+print "Here is some of the info about #{answer}: it's age is #{planets[answer].age} billion years, "
+print "radius is #{planets[answer].radius} miles, "
+puts "the distance from \n the sun is #{planets[answer].distance_from_the_sun} miles."
 
