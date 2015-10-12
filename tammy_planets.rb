069a632ef12f38a4f911d7eb42dcd1aa4d7394a8
smurfik <smurfik@mlpinit.com>
2015-10-12 13:51:53 -0700

    add class Planet

---
+++ tammy_planets.rb
@@ -0,0 +1,22 @@
+class Planet
+
+  def initialize(name, radius, mass)
+    @name = name
+    @radius = radius
+    @mass = mass
+  end
+
+  def name
+    @name
+  end
+
+  def radius
+    @radius
+  end
+
+  def mass
+    @mass
+  end
+
+end
+
