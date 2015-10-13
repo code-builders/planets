280167ca1ec18410e8b55a6d0f36bf718a1c4b53
LeShoyaStarkey <olstarkey@gmail.com>
2015-10-13 16:29:29 -0700

    Phase 1 complete without word formatting

--- leshoya.rb
+++ leshoya.rb
@@ -1,5 +1,9 @@
 # PRODUCT HAS A NAME, DIAMETER, MASS, MOONS, GRAVITY, AND DISTANCE FROM THE SUN VALUES
-class Planet
+class Planet #always camel case
+
+  attr_reader :name, :diameter, :mass, :moons, :gravity, :distance_from_the_sun
+  attr_writer :name, :diameter, :mass, :moons, :gravity, :distance_from_the_sun
+
 
   def initialize (name, diameter, mass, moons,gravity, distance_from_the_sun)
 
@@ -12,41 +16,138 @@ class Planet
 
   end
 
-  # earth = Planet.new("earth", "12", "18", "24", "40", "60")
-  # puts earth.name
-  # aSong.durationInMinutes → 4.333333333
-  # aSong.durationInMinutes = 4.2
-  # aSong.duration
-
-  # def health=(new_health)
-  #     @health = new_health
-  # end
-
-def name
-  @name
-
 end
 
-def diameter
-  @diameter
-end
+# ​
+#   def initialize(attrs) #2
+#     # attrs = {name: "Earth", mass: 9838}
+#     @name = attrs[:name] #3
+#     @diameter = attrs[:diameter]
+#   end
+# ​
+#   def name #5
+#     @name #6
+#   end
+# ​
+#   def diameter
+#     @diameter
+#   end
+# end
 
-def mass
-  @mass
+# # **SETTER METHOD ABOVE WHEN YOU PUT IN =NEW_NAME ***DONT DELETE
 
-end
+puts "Choose a Planet From the List:Mercury, Venus, or Earth"
+command=gets.chomp
 
-def moons
-  @moons
-end
 
-def gravity
-  @gravity
-end
+earth =   Planet.new("earth", "12", "18", "24", "40", "20 miles")
+mercury = Planet.new("Mercury", "19", "30", "24", "40", "60 miles")
+venus=    Planet.new("Venus", "19", "3", "12", "82", "90 miles")
 
-def distance_from_the_sun
-  @distance_from_the_sun
-end
+ if command == "Earth".downcase
+   puts "This is the value of earth"
+  puts earth.name
+  puts earth.diameter
+  puts earth.mass
+  puts earth.moons
+  puts earth.gravity
+  puts earth.distance_from_the_sun
+
+
+elsif command == "Mercury".downcase
+  puts "This is the value of Mercury"
+  puts mercury.name
+  puts mercury.diameter
+  puts mercury.mass
+  puts mercury.moons
+  puts mercury.gravity
+  puts mercury.distance_from_the_sun
+
+elsif
+  command == "Venus".downcase
+  puts "This is the value of Mercury"
+  puts venus.name
+  puts venus.diameter
+  puts venus.mass
+  puts venus.moons
+  puts venus.gravity
+  puts venus.distance_from_the_sun
+
+
+ else
+  puts "Bye bye"
+
+ end
+
+
+ puts "Give me another planet"
+ command2=gets.chomp
+ puts "bye bye"
+
+
+
+
+
+
+
+
+
+# ​
+# class Planet
+# ​
+#   def initialize(attrs) #2
+#     # attrs = {name: "Earth", mass: 9838}
+#     @name_var = attrs[:name_key] #3
+#     @mass_var = attrs[:mass_key]
+#   end
+# ​
+#   def name_method #5
+#     @name_var #6
+#   end
+# ​
+#   def mass_method
+#     @mass_var
+#   end
+# end
+# ​
+# class SolarSystem
+# ​
+#   def initialize(planets)
+#     # attrs = "Pineapple"
+#     @planets_var = planets
+#   end
+# ​
+#   def planets_method
+#     @planets_var
+#   end
+#
+#
+# end
+# ​
+# earth_hash = {name_key:"earth", diameter:"12", mass:"18",moons: "24", gravity:"40", distance_from_the_sun:"20 miles"}
+# earth = Planet.new(earth_hash) #1
+# ​
+# mercury_hash = {name_key:"Mercury", diameter:"19", mass:"30",moons: "24", gravity:"40", distance_from_the_sun: "60 miles"}
+# mercury = Planet.new(mercury_hash)
+# ​
+# array_of_planets = [earth, mercury]
+#
+# ss = SolarSystem.new(array_of_planets)
+# ss.planets_method
+# ​
+# puts ss.planets_method.first.name
+# puts ss.planets_method.last.name
+
+​
+# ss => SolarSystem instance <#SolarSystem:234234234>
+# .planets_method => Array []
+# .first => Planet <#Planet:234234234>
+# .name => String "Earth
+
+
+
+#
+
 
 # def in_stock?
 #   @qty_in_stock >0
@@ -64,84 +165,12 @@ end
 #   false
 # end
 
-
-
-
-end
 ##USE HASH LOGIC FROM BELOW TO PUT IN CODE#####
 
 #p=Product.new("Cool Gloves", 4999,1000)
-earth = Planet.new("earth", "12", "18", "24", "40", "20 miles")
-puts"This is the value of earth"
-puts earth.name
-puts earth.diameter
-puts earth.mass
-puts earth.moons
-puts earth.gravity
-puts earth.distance_from_the_sun
-
-mercury = Planet.new("Mercury", "19", "30", "24", "40", "60 miles")
-puts mercury.name
-puts mercury.diameter
-puts mercury.mass
-puts mercury.moons
-puts mercury.gravity
-puts mercury.distance_from_the_sun
+#
 
 
 
 
-# a={name:"Mercury",diameter: "12", mass:"4", moons:"8", gravity:"9", distance_from_the_sun: "12 miles"}
-# b={name:"Earth",diameter: "30", mass:"20", moons:"77", gravity:"18", distance_from_the_sun: "25 miles"}
-# c={name:"Venus",diameter: "29", mass:"7", moons:"2", gravity:"45", distance_from_the_sun: "120 miles"}
-# d={name:"Mars",diameter: "45", mass:"90", moons:"3", gravity:"62", distance_from_the_sun: "350 miles"}
-# e={name:"Jupiter",diameter: "88", mass:"22", moons:"47", gravity:"8", distance_from_the_sun: "1000 miles"}
 #
-#
-# puts "Which planet would you like to learn about?"
-# puts "Please enter a planet from the list: Mercury, Earth, Venus, Mars, Jupiter"
-# n=gets.chomp
-#
-# if n=="Mercury".downcase
-#   puts "Name of Planet: #{a[:name]},
-#       the diameter: #{a[:diameter]}
-#       the mass: #{a[:mass]}
-#       the number of moons: #{a[:moons]}
-#       the gravity: #{a[:gravity]}
-#       the distance from sun #{a[:distance_from_the_sun]}"
-# elsif n=="Earth".downcase
-#   puts "Name of Planet: #{b[:name]}
-#     the diameter: #{b[:diameter]}
-#     the mass: #{b[:mass]}
-#     the number of moons: #{b[:moons]}
-#     the gravity: #{b[:gravity]}
-#     the distance from sun #{b[:distance_from_the_sun]}"
-# elsif n=="Venus".downcase
-#   puts "Name of Planet: #{c[:name]}
-#    the diameter: #{c[:diameter]}
-#    the mass: #{c[:mass]}
-#    the number of moons: #{c[:moons]}
-#    the gravity: #{c[:gravity]}
-#    the distance from sun #{c[:distance_from_the_sun]}"
-# elsif n=="Mars".downcase
-#   puts "Name of Planet: #{d[:name]}
-#    the diameter: #{d[:diameter]}
-#    the mass: #{d[:mass]}
-#    the number of moons: #{d[:moons]}
-#    the gravity: #{d[:gravity]}
-#    the distance from sun #{d[:distance_from_the_sun]}"
-# elsif n=="Jupiter".downcase
-#   puts "Name of Planet: #{e[:name]}
-#   the diameter: #{e[:diameter]}
-#   the mass: #{e[:mass]}
-#   the number of moons: #{e[:moons]}
-#   the gravity: #{e[:gravity]}
-#    the distance from sun #{e[:distance_from_the_sun]}"
-#
-#
-# end
-#
-# puts "Give Me Another Planet"
-# a=gets.chomp
-# puts a
-# end
